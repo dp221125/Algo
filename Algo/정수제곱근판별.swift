2e4080743be2872b6e03eb800e182a2be2ded4d3
Milkyo <dp221125@naver.com>
2020-03-31 14:56:53 +0900

    정수 제곱근 판별 추가

--- Algo/정수제곱근판별.swift
+++ Algo/정수제곱근판별.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  정수제곱근판별.swift
 //  Algo
 //
-//  Created by Seokho on 2020/03/30.
+//  Created by Seokho on 2020/03/31.
 //  Copyright © 2020 Seokho. All rights reserved.
 //
 
@@ -11,5 +11,3 @@ import Foundation
 func solution(_ n:Int64) -> Int64 {
     return Int64(sqrt(Double(n)) == floor(sqrt(Double(n))) ? Int(pow(sqrt(Double(n)) + 1, 2)) : -1)
 }
-
-print(solution(121))
