d5197113f14861763ff039c54384d623dc7f2edd
Milkyo <dp221125@naver.com>
2020-03-31 15:21:43 +0900

    짝수와홀수 추가

---
+++ Algo/짝수와홀수.swift
@@ -0,0 +1,13 @@
+//
+//  짝수와홀수.swift
+//  Algo
+//
+//  Created by Seokho on 2020/03/31.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
