73a0d06cf989ceda277d32354059a6e249d1ac47
Milkyo <dp221125@naver.com>
2020-04-02 14:25:22 +0900

    Fix Bug: Color Error

--- Algo/main.swift
+++ Algo/main.swift
@@ -8,12 +8,35 @@
 
 import Foundation
 
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b) = (n[0], n[1])
+import Foundation
 
-for _ in 0..<b {
-    for _ in 0..<a {
-        print("*", terminator: "")
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var stage = Array(repeating: 0, count: N+2)
+    var pendingUser = Array(repeating: 0, count: N+2)
+    var failure: [(Double, Int)] = Array()
+    var result: [Int] = []
+    for s in stages {
+        for i in 1...s {
+            stage[i] += 1
+        }
+        pendingUser[s] += 1
     }
-    print()
+
+    for (i,s) in stage[1..<N+1].enumerated() {
+        failure.append((Double(pendingUser[i+1]) / Double(s),i+1))
+    }
+
+    failure.sort { a,b in
+        if a.0 != b.0 {
+            return a.0 > b.0
+        } else {
+            return a.1 < b.1
+        }
+    }
+
+    for f in failure {
+        result.append(f.1)
+    }
+
+    return result
 }
