addf281bbd594de78c87fb8c90981b5b6df3167a
Milkyo <dp221125@naver.com>
2020-04-02 17:06:11 +0900

    실패율 추가

--- Algo/실패율.swift
+++ Algo/실패율.swift
@@ -1,50 +1,10 @@
 //
-//  main.swift
+//  실패율.swift
 //  Algo
 //
-//  Created by Seokho on 2020/03/30.
+//  Created by Seokho on 2020/04/02.
 //  Copyright © 2020 Seokho. All rights reserved.
 //
-
-import Foundation
-
-/*
- var stage = Array(repeating: 0, count: N+2)
- var pendingUser = Array(repeating: 0, count: N+2)
- var failure: [(Double, Int)] = Array()
- var result: [Int] = []
- for s in stages {
- for i in 1...s {
- stage[i] += 1
- }
- pendingUser[s] += 1
- }
- 
- print(stage)
- print(pendingUser)
- 
- print(failure)
- for (i,s) in stage[1..<N+1].enumerated() {
- failure.append((Double(pendingUser[i+1]) / Double(s),i+1))
- print(failure)
- }
- 
- print(failure)
- 
- failure.sort { a,b in
- if a.0 != b.0 {
- return a.0 > b.0
- } else {
- return a.1 < b.1
- }
- }
- 
- for f in failure {
- result.append(f.1)
- }
- 
- return result
- */
 
 import Foundation
 
