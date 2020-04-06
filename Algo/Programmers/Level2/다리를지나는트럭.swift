e62cba2db434dc5572fe05e37a3d4a25c6a65e06
Milkyo <dp221125@naver.com>
2020-04-06 11:31:40 +0900

    다리를지나는 트럭 수정

--- Algo/Programmers/Level2/다리를지나는트럭.swift
+++ Algo/Programmers/Level2/다리를지나는트럭.swift
@@ -8,31 +8,33 @@
 
 import Foundation
 
-var onBridege = [Int]()
-var waitTrucks = truck_weights
-var cWeight = weight
-var times = [Int]()
-var result = 0
-
-while !onBridege.isEmpty || !waitTrucks.isEmpty {
-    result += 1
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var onBridege = [Int]()
+    var waitTrucks = truck_weights
+    var cWeight = weight
+    var times = [Int]()
+    var result = 0
     
-    let time: Int? = times.first
-    
-    if time != nil && bridge_length == result - time! {
-        cWeight += onBridege.removeFirst()
-        times.removeFirst()
+    while !onBridege.isEmpty || !waitTrucks.isEmpty {
+        result += 1
+        
+        let time: Int? = times.first
+        
+        if time != nil && bridge_length == result - time! {
+            cWeight += onBridege.removeFirst()
+            times.removeFirst()
+        }
+        
+        let truck: Int? = waitTrucks.first
+        
+        if truck != nil && cWeight >= truck! {
+            cWeight -= truck!
+            onBridege.append(truck!)
+            times.append(result)
+            waitTrucks.removeFirst()
+        }
+        
     }
     
-    let truck: Int? = waitTrucks.first
-    
-    if truck != nil && cWeight >= truck! {
-        cWeight -= truck!
-        onBridege.append(truck!)
-        times.append(result)
-        waitTrucks.removeFirst()
-    }
-    
+    return result
 }
-
-return result
