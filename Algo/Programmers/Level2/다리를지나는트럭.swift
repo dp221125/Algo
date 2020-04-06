ee3c20dfb7c86b81b18805e230341024f1de62c7
Milkyo <dp221125@naver.com>
2020-04-06 11:17:00 +0900

    다리를 지나는 트럭 추가

---
+++ Algo/Programmers/Level2/다리를지나는트럭.swift
@@ -0,0 +1,38 @@
+//
+//  다리를지나는트럭.swift
+//  Algo
+//
+//  Created by Seokho on 2020/04/06.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import Foundation
+
+var onBridege = [Int]()
+var waitTrucks = truck_weights
+var cWeight = weight
+var times = [Int]()
+var result = 0
+
+while !onBridege.isEmpty || !waitTrucks.isEmpty {
+    result += 1
+    
+    let time: Int? = times.first
+    
+    if time != nil && bridge_length == result - time! {
+        cWeight += onBridege.removeFirst()
+        times.removeFirst()
+    }
+    
+    let truck: Int? = waitTrucks.first
+    
+    if truck != nil && cWeight >= truck! {
+        cWeight -= truck!
+        onBridege.append(truck!)
+        times.append(result)
+        waitTrucks.removeFirst()
+    }
+    
+}
+
+return result
