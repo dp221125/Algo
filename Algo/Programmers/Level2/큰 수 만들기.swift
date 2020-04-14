21bf9a2e09dbffa75848a6898141404d8b46a9fa
Milkyo <dp221125@naver.com>
2020-04-14 11:28:08 +0900

    큰 수 만들기 추가

---
+++ Algo/Programmers/Level2/큰 수 만들기.swift
@@ -0,0 +1,42 @@
+//
+//  큰 수 만들기.swift
+//  Algo
+//
+//  Created by Seokho on 2020/04/14.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    var temp = [String]()
+    var result = ""
+    var cK = k
+    
+    for (index, num) in Array(number).enumerated() {
+        
+        while !temp.isEmpty && Int(temp.last!)! < Int(String(num))! && cK > 0 {
+            temp.removeLast()
+            cK -= 1
+        }
+        
+        if cK == 0 {
+            let sindex = number.index(number.startIndex, offsetBy: index)
+            let str = String(number[sindex ..< number.endIndex])
+            temp.append(str)
+            break
+        }
+        
+        temp.append(String(num))
+    }
+    
+    
+    if cK > 0 {
+        let sindex = number.index(number.startIndex, offsetBy: number.count - cK)
+        result = String(number[number.startIndex ..< sindex])
+    } else {
+        result = temp.joined()
+    }
+    
+    return result
+}
